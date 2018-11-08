734f7265b552e09b2680f1e879914c6681565088
Daniyar Kazbek <daniyar.kazbek@nu.edu.kz>
2018-11-08 19:46:36 +0600

    +2 controllers

--- src/main/kotlin/kz/scope/hiremeserver/controller/JobOfferController.kt
+++ src/main/kotlin/kz/scope/hiremeserver/controller/JobOfferController.kt
@@ -39,7 +39,7 @@ class JobOfferController {
                 jobOffer.role, jobOffer.company.id, jobOffer.jobType, jobOffer.createdAt, jobOffer.updatedAt)
     }
 
-    @GetMapping("/job-offer/find-by-company")
+    @GetMapping("/job-offers/find-by-company")
     @PreAuthorize("hasRole('USER')")
     fun getJobOfferByCompany(@RequestParam(value = "company_id", required = true) company_id: Long)
             : List<JobOfferResponse> {
@@ -54,8 +54,39 @@ class JobOfferController {
         }
 
         val jobOffers = jobOfferRepository.findByCompany(company)
-        val jobOfferResponses: MutableList<JobOfferResponse>
-        jobOfferResponses = ArrayList<JobOfferResponse>()
+        val jobOfferResponses: MutableList<JobOfferResponse> = ArrayList<JobOfferResponse>()
+
+        for (jobOffer in jobOffers) {
+            jobOfferResponses.add(JobOfferResponse(jobOffer.id, jobOffer.descriptionOfResponsibilities, jobOffer.skills,
+                    jobOffer.role, jobOffer.company.id, jobOffer.jobType, jobOffer.createdAt, jobOffer.updatedAt))
+        }
+
+        return jobOfferResponses
+    }
+
+    @GetMapping("/job-offers/find-by-role")
+    @PreAuthorize("hasRole('USER')")
+    fun getJobOfferByRole(@RequestParam(value = "role", required = true) role: String)
+            : List<JobOfferResponse> {
+
+        val jobOffers = jobOfferRepository.findByRole(role)
+        val jobOfferResponses: MutableList<JobOfferResponse> = ArrayList<JobOfferResponse>()
+
+        for (jobOffer in jobOffers) {
+            jobOfferResponses.add(JobOfferResponse(jobOffer.id, jobOffer.descriptionOfResponsibilities, jobOffer.skills,
+                    jobOffer.role, jobOffer.company.id, jobOffer.jobType, jobOffer.createdAt, jobOffer.updatedAt))
+        }
+
+        return jobOfferResponses
+    }
+
+    @GetMapping("/job-offers/find-by-skills")
+    @PreAuthorize("hasRole('USER')")
+    fun getJobOfferBySkills(@RequestParam(value = "skills", required = true) skills: String)
+            : List<JobOfferResponse> {
+
+        val jobOffers = jobOfferRepository.findBySkills(skills)
+        val jobOfferResponses: MutableList<JobOfferResponse> = ArrayList<JobOfferResponse>()
 
         for (jobOffer in jobOffers) {
             jobOfferResponses.add(JobOfferResponse(jobOffer.id, jobOffer.descriptionOfResponsibilities, jobOffer.skills,
