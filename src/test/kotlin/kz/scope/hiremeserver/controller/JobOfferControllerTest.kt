7ad8cd1a359cae1e265abb0a0cf35a458aa561bf
chromomons <chromomons@gmail.com>
2018-11-10 11:53:38 +0600

    refactored user info, merged with posts, added viewing 10 latest posts

---
+++ src/test/kotlin/kz/scope/hiremeserver/controller/JobOfferControllerTest.kt
@@ -0,0 +1,17 @@
+package kz.scope.hiremeserver.controller
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class JobOfferControllerTest {
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+}
---
+++ src/test/kotlin/kz/scope/hiremeserver/controller/UserControllerTest.kt
@@ -0,0 +1,17 @@
+package kz.scope.hiremeserver.controller
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class UserControllerTest {
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+}
